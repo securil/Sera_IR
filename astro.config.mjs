c4f5c93864031e7ffc8f522f0d554924bff7cc9a
securil <securil@naver.com>
2025-05-08 14:28:48 +0900

    GitHub Pages 배포를 위한 설정 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://securil.github.io',
+  base: '/Sera_IR',
+});
